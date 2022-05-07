ef0f3070cb2b7216190b899e6608fdbb473918e2
Wanlin Li <wanlinli2018@gmail.com>
2022-05-07 08:22:41 -0400

    delete test

--- Python/apps/usingOurDataset.py
+++ Python/apps/usingOurDataset.py
@@ -47,15 +47,15 @@ layout = dbc.Container([
                             editable=True,              # allow editing of data inside all cells
                             filter_action="native",     # allow filtering of data by user ('native') or not ('none')
                             sort_action="native",       # enables data to be sorted per-column by user or not ('none')
-                            sort_mode="single",         # sort across 'multi' or 'single' columns
+                            sort_mode="multi",         # sort across 'multi' or 'single' columns
                             column_selectable="multi",  # allow users to select 'multi' or 'single' columns
-                            #row_selectable="multi",     # allow users to select 'multi' or 'single' rows
+                            row_selectable="multi",     # allow users to select 'multi' or 'single' rows
                             row_deletable=False,         # choose if user can delete a row (True) or not (False)
                             selected_columns=[],        # ids of columns that user selects
-                            #selected_rows=[],           # indices of rows that user selects
+                            selected_rows=[],           # indices of rows that user selects
                             page_action="native",       # all data is passed to the table up-front or not ('none')
                             page_current=0,             # page number that user is on
-                            page_size=6,                # number of rows visible per page
+                            page_size=100,                # number of rows visible per page
                             style_cell={                # ensure adequate header width when text is shorter than cell's text
                                 'minWidth': 95, 'maxWidth': 95, 'width': 95
                             },
